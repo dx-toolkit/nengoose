10e1d4ceea5348ee856b601cb142af64e33a9146
asp143 <ralphmungcal09@gmail.com>
2019-06-19 09:27:04 +0800

    Added docs

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,68 @@
 declare namespace nengoose {
     class Db {
         constructor(database: string);
+
+        /**
+         * 
+         * @param schema 
+         */
         Schema(schema: any): Model;
     
+        /**
+         * Returns the string location of the database
+         */
         getDatabaseLocation(): string;
     }
 
     class Model {
         constructor(database: string, schema: any);
+        /**
+         * 
+         * @param newData 
+         */
         Create(newData): Promise<any>;
-        FindById(id: number): Promise<any>;
-        FindAll(query, options): Promise<any>;
+        
+
+        /**
+         * 
+         * @param id 
+         */
+        FindById(id: string): Promise<any>;
+
+
+        /**
+         * 
+         * @param query 
+         * @param options 
+         */
+        FindAll(query, options?): Promise<any>;
+
+        /**
+         * 
+         * @param IdList 
+         */
         FindByIdList(IdList: Array<any>): Promise<any>;
+        
+        /**
+         * @param id
+         * @param options
+         */
         DeleteById(id, options): Promise<any>;
+        
+        /**
+         * 
+         * @param query 
+         * @param newData 
+         * @param options 
+         */
         Update(query, newData, options): Promise<any>;
+        
+        /**
+         * 
+         * @param id 
+         * @param newData 
+         * @param options 
+         */
         UpdateById(id, newData, options): Promise<any>;
     }
 }
